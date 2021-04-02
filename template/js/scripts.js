198ea44cdd786c1babfb8185e864ce9bf6d2b390
thewritingdude <pippero_lo_smilzo@hotmail.com>
2021-04-02 21:19:27 +0200

    Added loader

--- template/js/scripts.js
+++ template/js/scripts.js
@@ -124,7 +124,14 @@ function loadMedia() {
         }
 
         document.querySelector('.container').style.display = "block";
+        document.querySelector(".loader").style.display = "none";
     });
+
+    preload.onprogress = event => {
+        document.querySelector(".progress-bar").style.width = event.progress+'%';
+        document.querySelector(".percentage").textContent = event.progress+'%';
+        document.querySelector(".progress-bar")["aria-valuenow"]= event.progress;
+    }    
 
 }
 
